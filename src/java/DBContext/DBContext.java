dab8439552df0069f335df96711819dc8b7e5494
Glorlicon <83930658+Glorlicon@users.noreply.github.com>
2022-03-06 20:13:23 +0700

    Added Login and Main home page
    Both page were created using Bootstrap and jQuery

---
+++ src/java/DBContext/DBContext.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DBContext;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author BK
+ */
+public class DBContext {
+
+    protected Connection connection;
+
+    public DBContext() {
+        try {
+            String user = "SE1611";
+            String pass = "123";
+            String url = "jdbc:sqlserver://LAPTOP-K6CU2T3T\\SQLEXPRESS:1433;databaseName=Workshop1_SP2022";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
